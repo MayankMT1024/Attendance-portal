7db39d13d82ad0688a1b9ee28f69f3433ac07eb7
Mayank <tmayank676@gmail.com>
2026-09-25 00:44:12 +0530

    Remove legacy pages and unused student teacher records

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('testBtn').onclick = async () => {
-  const res = await fetch('/api/hello');
-  const data = await res.json();
-  document.getElementById('result').innerText = data.message;
-};
